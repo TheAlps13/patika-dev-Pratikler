f6c538de747b6f4f9bde15e28851765bb588ac49
Alper Taşçı <alper.tasci@hotmail.com>
2021-12-28 10:04:59 +0300

    opeatorler odevi eklendi

---
+++ operatorler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Atama ve işlemli atama
+            int x = 5;
+            int y = 15;
+            
+            Console.WriteLine(y);
+            y += x;
+            Console.WriteLine(y);
+            y -= x;
+            Console.WriteLine(y);
+            y *= x;
+            Console.WriteLine(y);
+            y /= x;
+            Console.WriteLine(y);
+            y %= x;
+            Console.WriteLine(y);
+        }
+    }
+}
