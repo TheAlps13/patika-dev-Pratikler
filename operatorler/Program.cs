33e8f8b57ca5ed62bf094962f6643aafbd3226da
unknown <alper.tasci@hotmail.com>
2021-12-28 16:49:46 +0300

    Operatorler odevinin kalani eklendi

--- operatorler/Program.cs
+++ operatorler/Program.cs
@@ -6,7 +6,7 @@ namespace operatorler
     {
         static void Main(string[] args)
         {
-           //Atama ve işlemli atama
+           // Atama ve işlemli atama
             int x = 5;
             int y = 15;
             
@@ -21,6 +21,65 @@ namespace operatorler
             Console.WriteLine(y);
             y %= x;
             Console.WriteLine(y);
+
+            // Mantıksal operatörlerle işlemli atama
+            bool yeaTrue = false;
+            bool notTrue = true;
+            // Mantıklı atama
+            notTrue ^= yeaTrue; // xor
+            notTrue |= yeaTrue; // or
+            notTrue &= yeaTrue; // and
+            // Tekrar ilk değerlerine dönüyoruz
+            yeaTrue = true;
+            notTrue = false;
+            // Mantıksal operatörler
+            Console.WriteLine("**Mantıksal Operatörler**");
+            if(yeaTrue && notTrue)
+                Console.WriteLine("Both are true");
+            
+            if(yeaTrue || notTrue)
+                Console.WriteLine("One of us true");
+
+            if(yeaTrue || !notTrue)
+                Console.WriteLine("One of us is not true but we gave him a chance :)");
+            if(yeaTrue ^ notTrue)
+                Console.WriteLine("One of us different");
+
+
+            Console.WriteLine("**İlişkisel Operatörler**");
+            // İlişkisel operatörler
+            // <, >, <=, >=, ==, !=
+            bool sonuc = false;
+            int a = 5, b = 4;
+
+            sonuc = a > b;
+            Console.WriteLine(sonuc);
+            sonuc = a < b;
+            Console.WriteLine(sonuc);
+            sonuc = a <= b;
+            Console.WriteLine(sonuc);
+            sonuc = a >= b;
+            Console.WriteLine(sonuc);
+            sonuc = a == b;
+            Console.WriteLine(sonuc);
+            sonuc = a != b;
+            Console.WriteLine(sonuc);
+
+            Console.WriteLine("**Aritmetik Operatörler**");
+            // /, *, +, - , %
+
+            int num1 = 10, num2 = 20, sonuc2 = num2 + num1;
+            Console.WriteLine(sonuc2);
+
+            sonuc2 = num1 - num2;
+            Console.WriteLine(sonuc2);
+            sonuc2 = num1 / num2;
+            Console.WriteLine(sonuc2);
+            sonuc2 = num1 * num2;
+            Console.WriteLine(sonuc2);
+            sonuc2 = num1 % num2;
+            Console.WriteLine(sonuc2);
+            
         }
     }
 }
