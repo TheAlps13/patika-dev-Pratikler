aaa48822221c00ada5c1b94159c61b5e48d82d62
unknown <alper.tasci@hotmail.com>
2022-01-02 00:09:38 +0300

    Donguler-while-foreach odevi eklendi

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 den başlayarak girilen sayıya kadar(sayı dahil) ortalamasını veren program
+            Console.WriteLine("Lütfen sayiyi giriniz");
+
+            int sayac = 1;
+            int sayi = int.Parse(Console.ReadLine());
+            int toplam = 0;
+
+            while(sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+
+            Console.WriteLine("Ortalama: " + toplam / sayi);
+
+            // a dan z ye kadar tüm harfleri konsola yazdıran program
+            char ch = 'a';
+
+            while(ch <= 'z')
+            {
+                Console.WriteLine(ch);
+                ch++;
+            }
+            
+            Console.WriteLine("\n****FOR EACH STARTS HERE*****");
+
+            string[] arabalar = {"BMW", "Honda", "One Beygir", "Bugatti"};
+            foreach(string str in arabalar)
+                Console.WriteLine(str);
+        }
+    }
+}
