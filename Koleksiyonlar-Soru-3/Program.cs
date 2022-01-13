78881759c8acdff85a27a3ec984ea09f5a89ec56
unknown <alper.tasci@hotmail.com>
2022-01-13 14:10:21 +0300

    Koleksiyonlar-Soru-3 odevi eklendi

---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cumle yaziniz lutfen...");
+            string sesliHarfler = "aeıioöuü";
+
+            ArrayList bulunanSesliHarfler = new ArrayList();
+
+            string cumle = Console.ReadLine();
+            foreach (char ch in cumle)
+                if (sesliHarfler.Contains(ch))
+                    bulunanSesliHarfler.Add(ch);
+
+            Console.WriteLine("Cumlede bulunan sesli harfler: ");
+            bulunanSesliHarfler.Sort();
+            foreach (char ch in bulunanSesliHarfler)
+                Console.WriteLine(ch);
+        }
+    }
+}
---
+++ Koleksiyonlar-Soru-3/README.md
@@ -0,0 +1,3 @@
+### Koleksiyonlar soru 3
+
+C# 101 Koleksiyonlar 3. soru çözümü
