75ed1ab329219dce44cc6c329becc5395d4f24b9
Alper <alper.tasci@hotmail.com>
2022-01-17 17:05:18 +0300

    Inheritence dersi eklendi

---
+++ inheritence/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace inheritence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TohumluBitkiler tohumluBitki = new TohumluBitkiler();
+            tohumluBitki.TohumlaCogalma();
+
+            Kuslar marti = new Kuslar();
+            marti.Adaptasyon();
+            marti.Ucmak();
+        }
+    }
+}
